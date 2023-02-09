a685f3f8e68952183956c7971d7c02d8d47b1d8a
Mohamed Adel <124937844+MuhammedAdell1@users.noreply.github.com>
2023-02-10 00:53:06 +0200

    Uploading the front end

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button'
+import { KonvaModule } from "ng2-konva";
+import { KonvaService } from './konva.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    KonvaModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    KonvaService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
